ec4773b064ce360a23b9c319137df6717f815dad
carlosmspk <carlos_cp_santos@hotmail.com>
2021-07-18 01:18:11 +0100

    Hello World!

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+"""
+Just like any other first program, we need to print the good ol' Hello World!
+
+PS: The above sentence was autocompleted by Copilot after "go-" from "good"
+PSS: I didn't even need to write the comment for Print "Hello World". As soon
+as I wrote the "#" for comment, Copilot the comment.
+"""
+
+# Print "Hello World!"
+print("Hello World!")
